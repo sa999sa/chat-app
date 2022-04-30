6e565e55ce1c94843be65b4b7320c7a70d50d7de
sa999na <lumimosita999@icloud.com>
2022-04-30 23:01:58 +0900

    real timeのチャットが可能

---
+++ client.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"log"
+
+	"github.com/gorilla/websocket"
+)
+
+type client struct {
+	socket *websocket.Conn
+	send   chan []byte
+	room   *room
+}
+
+func (c *client) read() {
+	log.Println("呼ばれたよread")
+	for {
+		if _, msg, err := c.socket.ReadMessage(); err == nil {
+			c.room.forward <- msg
+		} else {
+			break
+		}
+	}
+	c.socket.Close()
+}
+func (c *client) write() {
+	log.Println("呼ばれたよwrite")
+	for msg := range c.send {
+		if err := c.socket.WriteMessage(websocket.TextMessage, msg); err != nil {
+			break
+		}
+	}
+	c.socket.Close()
+}
--- main.go
+++ main.go
@@ -1,11 +1,12 @@
 package main
 
 import (
-	"html/template"
+	"flag"
 	"log"
 	"net/http"
 	"path/filepath"
 	"sync"
+	"text/template"
 )
 
 type templateHandler struct {
@@ -19,12 +20,18 @@ func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		t.temp1 =
 			template.Must(template.ParseFiles(filepath.Join("templates", t.filename)))
 	})
-	t.temp1.Execute(w, nil)
+	t.temp1.Execute(w, r)
 }
 
 func main() {
+	var addr = flag.String("addr", ":8080", "アプリケーションのアドレス")
+	flag.Parse()
+	r := newRoom()
 	http.Handle("/", &templateHandler{filename: "chat.html"})
+	http.Handle("/room", r)
+	go r.run()
+	log.Println("サーバーを開始します。ポート:", *addr)
 	if err := http.ListenAndServe(":8080", nil); err != nil {
-		log.Fatal("ListenAndServer")
+		log.Fatal("ListenAndServer", err)
 	}
 }
